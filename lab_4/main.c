dd74cd00813327ecddaa701be29ecdc81ad72f50
Daniil Makhov <mahovdnl@gmail.com>
2025-01-07 04:34:28 +0300

    lab 4

--- lab_4/main.c
+++ lab_4/main.c
@@ -1,7 +1,83 @@
+#include "main.h"
 
+static alloc_create_func *alloc_create;
+static alloc_alloc_func *alloc_alloc;
+static alloc_free_func *alloc_free;
+static alloc_destroy_func *alloc_destroy;
 
-int main(){
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <library_path>\n", argv[0]);
+        return 1;
+    }
 
+    void *library = dlopen(argv[1], RTLD_LAZY);
+    if (!library) {
+        fprintf(stderr, "Error loading library: %s\n", dlerror());
+        return 1;
+    }
+
+    alloc_create = (alloc_create_func *)dlsym(library, "allocator_create");
+    if (!alloc_create) {
+        fprintf(stderr, "Error finding symbol: %s\n", dlerror());
+        return 1;
+    }
+
+    alloc_alloc = (alloc_alloc_func *)dlsym(library, "allocator_alloc");
+    if (!alloc_alloc) {
+        fprintf(stderr, "Error finding symbol: %s\n", dlerror());
+        return 1;
+    }
+
+    alloc_free = (alloc_free_func *)dlsym(library, "allocator_free");
+    if (!alloc_free) {
+        fprintf(stderr, "Error finding symbol: %s\n", dlerror());
+        return 1;
+    }
+
+    alloc_destroy = (alloc_destroy_func *)dlsym(library, "allocator_destroy");
+    if (!alloc_destroy) {
+        fprintf(stderr, "Error finding symbol: %s\n", dlerror());
+        return 1;
+    }
+
+    void *memory = mmap(NULL, 100000, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (memory == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    Allocator *allocator = alloc_create(memory, 1024 * 20);
+    if (!allocator) {
+        return 2;
+    }
+
+    char *allocated_mem = (char *) alloc_alloc(allocator, 500);
+    if (!allocated_mem) {
+        return 3;
+    }
+
+    char *allocated_mem2 = (char *) alloc_alloc(allocator, 32);
+    if (!allocated_mem2) {
+        return 3;
+    }
+
+    allocated_mem[0] = '1';
+    allocated_mem[1] = '\0';
+    allocated_mem2[0] = 'l';
+    allocated_mem2[1] = '\0';
+
+    write(STDOUT_FILENO, allocated_mem, 2);
+    write(STDOUT_FILENO, allocated_mem2, 2);
+
+    alloc_free(allocator, allocated_mem);
+    alloc_destroy(allocator);
+
+    if (munmap(memory, 100000) == -1) {
+        perror("munmap");
+    }
+
+    dlclose(library);
 
     return 0;
 }
---
+++ lab_4/mkk.c
@@ -0,0 +1,238 @@
+#include "mkk.h"
+
+Allocator *allocator_create(void *const memory, const size_t size) {
+    Allocator *new_allocator = (Allocator *) mmap(NULL, sizeof(Allocator), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (new_allocator == MAP_FAILED)
+        return NULL;
+
+    size_t k_sizes = size / PAGESIZE + (size % PAGESIZE != 0);
+    new_allocator->page_count = k_sizes;
+
+    new_allocator->kmemsizes = (Page *) mmap(NULL, sizeof(Page) * k_sizes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (new_allocator->kmemsizes == MAP_FAILED) {
+        munmap(new_allocator, sizeof(Allocator));
+        return NULL;
+    }
+
+    new_allocator->kmemsizes[0].start_addr = memory;
+    new_allocator->kmemsizes[0].page_size = PAGESIZE;
+    if (size / PAGESIZE <= 0)
+        new_allocator->kmemsizes[0].page_size = size % PAGESIZE;
+
+    new_allocator->kmemsizes[0].frag_size = 0;
+
+    new_allocator->free_page = (FreeList *) mmap(NULL, sizeof(FreeList), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (new_allocator->free_page == MAP_FAILED) {
+        munmap(new_allocator->kmemsizes, sizeof(Page) * k_sizes);
+        munmap(new_allocator, sizeof(Allocator));
+        return NULL;
+    }
+    new_allocator->free_page->page = &new_allocator->kmemsizes[0];
+
+    FreeList *freeList = new_allocator->free_page;
+
+    for (int i = 1; i < k_sizes; ++i) {
+        new_allocator->kmemsizes[i].page_size = PAGESIZE;
+        if (i == k_sizes - 1 && size % PAGESIZE > 0) {
+            new_allocator->kmemsizes[i].page_size = size % PAGESIZE;
+        }
+        new_allocator->kmemsizes[i].start_addr = new_allocator->kmemsizes[i - 1].start_addr + PAGESIZE;
+        new_allocator->kmemsizes[i].frag_size = 0;
+
+        freeList->next = (FreeList *) mmap(NULL, sizeof(FreeList), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        if (freeList->next == MAP_FAILED) {
+            // Cleanup and return NULL in case of failure
+            munmap(new_allocator->kmemsizes, sizeof(Page) * k_sizes);
+            munmap(new_allocator, sizeof(Allocator));
+            return NULL;
+        }
+        freeList = freeList->next;
+        freeList->page = &new_allocator->kmemsizes[i];
+    }
+
+    return new_allocator;
+}
+
+void *small_alloc(Allocator *const allocator, const size_t size) {
+    unsigned char order = NDX(size);
+
+    void *allocated_chunk;
+
+    // If free cells of this size exist, then use them
+    if (allocator->freelistarr[order]) {
+        allocated_chunk = allocator->freelistarr[order]->val;
+
+        Buffer *residue = allocator->freelistarr[order];
+
+        allocator->freelistarr[order] = allocator->freelistarr[order]->next;
+
+        munmap(residue, sizeof(Buffer));
+
+        return allocated_chunk;
+    }
+    // Else alloc page for this size
+
+    if (!allocator->free_page)
+        return NULL;
+
+    Page *free_page = allocator->free_page->page;
+    allocator->free_page = allocator->free_page->next;
+
+    free_page->frag_size = 1 << (MIN_ORDER + order);
+
+    int fragments = free_page->page_size / free_page->frag_size;
+
+    allocator->freelistarr[order] = (Buffer *) mmap(NULL, sizeof(Buffer), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    Buffer *current_buf = allocator->freelistarr[order];
+
+    allocated_chunk = free_page->start_addr;
+
+    current_buf->val = free_page->start_addr + free_page->frag_size;
+
+    // Add newly allocated fragments to the pool
+    for (int i = 2; i < fragments; ++i) {
+        current_buf->next = (Buffer *) mmap(NULL, sizeof(Buffer), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+        current_buf = current_buf->next;
+
+        current_buf->val = free_page->start_addr + free_page->frag_size * i;
+    }
+
+    return allocated_chunk;
+}
+
+void *large_alloc(Allocator *const allocator, const size_t size) {
+    // Check if contiguous buffers are ok (first fit)
+
+    size_t need_pages = size / PAGESIZE + (size % PAGESIZE != 0);
+
+    if (need_pages > allocator->page_count)
+        return NULL;
+
+    FreeList *cur = allocator->free_page, *prev = NULL;
+
+    int available = 0;
+    Page *available_start = NULL;
+
+    while (cur) {
+
+        if (cur->next && cur->next->page->start_addr - cur->page->page_size == cur->page->start_addr) {
+            available++;
+
+            if (!available_start)
+                available_start = cur->page;
+        } else {
+            available_start = NULL;
+            available = 0;
+        }
+
+        if (available >= need_pages)
+            break;
+
+        if (!available)
+            prev = cur;
+
+        cur = cur->next;
+    }
+
+    if (available >= need_pages) {
+        Page *allocated_page = available_start;
+        for (int i = 0; i < need_pages; ++i) {
+            allocated_page->frag_size = -1;
+            allocated_page = (Page *)((char*)allocated_page->start_addr + allocated_page->page_size);
+        }
+
+        if (prev)
+            prev->next = cur ? cur->next : NULL;
+        else
+            allocator->free_page = cur ? cur->next : NULL;
+
+        munmap(cur, sizeof(FreeList));
+        return available_start->start_addr;
+    }
+
+    return NULL;
+}
+
+void *allocator_alloc(Allocator *const allocator, const size_t size) {
+    if (size <= PAGESIZE / 2) {
+        return small_alloc(allocator, size);
+    }
+
+    return large_alloc(allocator, size);
+}
+
+void allocator_free(Allocator *const allocator, void *const memory) {
+    size_t memdiff = (memory - allocator->kmemsizes[0].start_addr);
+
+    size_t page_id = memdiff / PAGESIZE;
+    if (page_id >= allocator->page_count)
+        return;
+
+    Page *page = &allocator->kmemsizes[page_id];
+
+    int order = NDX(page->frag_size);
+
+    if (page->frag_size < PAGESIZE) {
+        Buffer *new_free_buffer = (Buffer *) mmap(NULL, sizeof(Buffer), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        if (!new_free_buffer)
+            return;
+
+        new_free_buffer->val = memory;
+
+        new_free_buffer->next = allocator->freelistarr[order];
+        allocator->freelistarr[order] = new_free_buffer;
+    } else {
+        page->frag_size = 0;
+
+        // Free contiguous pages
+
+        if (page_id >= allocator->page_count - 1)
+            return;
+
+        int i = 1;
+        Page *cur = &allocator->kmemsizes[page_id + i], *prev = page;
+
+        while (cur->frag_size == -1) {
+            cur->start_addr = prev->start_addr + prev->page_size;
+            cur->frag_size = 0;
+
+            prev = cur;
+
+            i++;
+            cur = &allocator->kmemsizes[page_id + i];
+        }
+    }
+
+}
+
+void allocator_destroy(Allocator *const allocator) {
+    for (int i = 0; i < MAX_ORDER - MIN_ORDER; ++i) {
+        destroy_buffer(allocator->freelistarr[i]);
+    }
+
+    destroy_free_pages_list(allocator->free_page);
+
+    munmap(allocator->kmemsizes, sizeof(Page) * allocator->page_count);
+    munmap(allocator, sizeof(Allocator));
+}
+
+
+void destroy_buffer(Buffer *start) {
+    Buffer *cur = start, *prev;
+    while (cur) {
+        prev = cur;
+        cur = cur->next;
+        munmap(prev, sizeof(Buffer));
+    }
+}
+
+void destroy_free_pages_list(FreeList *start) {
+    FreeList *cur = start, *prev;
+    while (cur) {
+        prev = cur;
+        cur = cur->next;
+        munmap(prev, sizeof(FreeList));
+    }
+}
