84265c2e382f77f2979055becbf44fc6c7f06a44
Daniil Makhov <mahovdnl@gmail.com>
2025-01-08 13:15:41 +0300

    lab 4 small minor fix

--- lab_4/buddy.h
+++ lab_4/buddy.h
@@ -20,11 +20,8 @@ typedef struct Allocator {
 } Allocator;
 
 Allocator* allocator_create(void* memory, size_t size);
-
 void* allocator_alloc(Allocator *allocator, size_t size);
-
 void allocator_free(Allocator *allocator, void *memory);
-
 void allocator_destroy(Allocator *allocator);
 
 #endif  // OPERATIONAL_SYSTEMS_BUDDY_H
--- lab_4/main.h
+++ lab_4/main.h
@@ -6,12 +6,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-// #include "buddy.h"
-#include "mkk.h"
+#include "buddy.h"
+// #include "mkk.h"
 
-typedef Allocator *(alloc_create_func)(void *memory, size_t size);
-typedef void *(alloc_alloc_func)(Allocator *allocator, size_t size);
-typedef void (alloc_free_func)(Allocator *allocator, void *memory);
-typedef void (alloc_destroy_func)(Allocator *allocator);
+typedef Allocator *(alloc_create_func)(void *const memory, const size_t size);
+typedef void *(alloc_alloc_func)(Allocator *const allocator, const size_t size);
+typedef void (alloc_free_func)(Allocator *const allocator, void *const memory);
+typedef void (alloc_destroy_func)(Allocator *const allocator);
 
 #endif // OPERATIONAL_SYSTEMS_MAIN_H
